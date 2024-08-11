bc155ad66afa6bd047990b9809e2fe27ed6e2767
sahhhall <sahalvavoor313@gmail.com>
2024-08-11 17:14:18 +0530

    setup query service for handle new data structre

--- comments/index.js
+++ comments/index.js
@@ -2,7 +2,7 @@ import express from "express";
 const app = express();
 import { randomBytes } from "crypto";
 import cors from "cors";
-
+import axios from 'axios';
 const corsOptions = {
     origin:"*",
 }
@@ -15,15 +15,29 @@ app.get("/posts/:id/comments", (req, res) => {
   res.send(commentsByPostId[req.params.id] || []);
 });
 
-app.post("/posts/:id/comments", (req, res) => {
+app.post("/posts/:id/comments", async(req, res) => {
   console.log("hi");
   const commentId = randomBytes(4).toString("hex");
   const { content } = req.body;
   const comments = commentsByPostId[req.params.id] || [];
   comments.push({ id: commentId, content });
   commentsByPostId[req.params.id] = comments;
+  await axios.post('http://localhost:4005/events', {
+    type: 'CommentCreated',
+    data: {
+      id: commentId,
+      content,
+      postId: req.params.id
+    }
+  })
   res.status(201).send(comments);
 });
+
+app.post('/events',(req,res)=> {
+  console.log('reciving event', req.body.type);
+  res.send({})
+})
+
 
 app.listen(4001, () => {
   console.log(`running on 4001`);
